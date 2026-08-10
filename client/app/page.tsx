058171213aa7b9eff0cca8d3621659d7064a87ca
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-10 17:29:55 +0300

    feat: add shadcn, nice components, praise King Jesus. also add a game grid

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,35 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useEffect, useState } from "react";
+
+export enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
 
 export default function IndexPage() {
   const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
-  const [result, setResult] = useState<string>("Waiting...");
+  const [direction, setDirection] = useState<Direction>(Direction.UP);
 
   const move = (e: KeyboardEvent) => {
     if (e.key == "ArrowUp") {
-      setResult("/\\");
+      setDirection(Direction.UP);
     } else if (e.key == "ArrowDown") {
-      setResult("\\/");
+      setDirection(Direction.DOWN);
     } else if (e.key == "ArrowLeft") {
-      setResult("<");
+      setDirection(Direction.LEFT);
     } else if (e.key == "ArrowRight") {
-      setResult(">");
+      setDirection(Direction.RIGHT);
     }
   };
 
@@ -25,22 +40,55 @@ export default function IndexPage() {
 
   useEffect(() => {
     setInterval(() => {
-      const countdown = window.setTimeout(move);
+      console.log("Jesus is KING");
     }, 1000);
   }, []);
 
-  if (!isGameStarted)
-    return (
-      <div className="p-3">
-        <button
-          className="cursor-pointer bg-sky-600 hover:bg-sky-700 text-white rounded-md p-2 active:scale-95"
-          onClick={() => setIsGameStarted(true)}
-        >
-          Почати
-        </button>
-      </div>
-    );
-  else if (isGameStarted) {
-    return <div className="grid grid-cols-12 grid-rows-12">{result}</div>;
-  }
+  return (
+    <div className="bg-sky-50 min-h-screen flex items-center justify-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="font-bold text-2xl">Грай в змійку</CardTitle>
+          <CardDescription>Це простенька гра в змійку</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {isGameStarted ? (
+            <div className="grid grid-cols-5 grid-rows-5 gap-1">
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+              <div className="bg-stone-100 rounded-md p-1 aspect-square"></div>
+            </div>
+          ) : (
+            <Button onClick={() => setIsGameStarted(true)}>Почати</Button>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
